b146961654b0664f1952f3983e2db478cafec30b
Hank Ehly <henry.ehly@gmail.com>
2019-11-04 14:30:28 +0900

    Upload assets to S3

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,43 @@
-const BundleTracker = require("webpack-bundle-tracker");
+const AWS = require("aws-sdk");
+// const BundleTracker = require("webpack-bundle-tracker");
+const S3Plugin = require('webpack-s3-plugin');
+const exec = require('child_process').exec;
+
+const gitsha = (path, callback) => {
+    exec('git rev-parse --short HEAD', {cwd: path}, (error, stdout, stderr) => {
+        if (error) {
+            return callback(error, stderr.trim());
+        }
+        callback(null, stdout.trim())
+    });
+};
 
 module.exports = {
+    filenameHashing: false,
     configureWebpack: {
         plugins: [
-            new BundleTracker({
-                path: __dirname,
-                filename: "./webpack-stats.json"
+            // new BundleTracker({
+            //     path: __dirname,
+            //     filename: "./webpack-stats.json"
+            // }),
+            new S3Plugin({
+                s3Options: {
+                    credentials: new AWS.SharedIniFileCredentials({profile: "default"})
+                },
+                s3UploadOptions: {
+                    Bucket: "octo-waffle",
+                },
+                basePathTransform: () => {
+                    return new Promise((resolve, reject) => {
+                        gitsha(__dirname, function (error, output) {
+                            if (error) {
+                                reject(error);
+                            } else {
+                                resolve(`ebcustom-${output}`);
+                            }
+                        });
+                    });
+                },
             })
         ]
     },
