b84e661c56fce1d774b46b611a168004e941cec7
Hank Ehly <henry.ehly@gmail.com>
2019-11-04 15:47:34 +0900

    Update deploy script

--- Makefile
+++ Makefile
@@ -1,3 +1,18 @@
 deploy:
+	# Output HEAD sha to file named "HEAD"
+	git rev-parse HEAD > HEAD
+
+	# Build production assets
 	npm run build -- --mode production
+
+	# Upload assets to S3
+	aws s3 sync ./dist s3://octo-waffle/ebcustom/$(cat ./HEAD)/
+
+	# Deploy new app version to beanstalk
+	# Bundle includes HEAD file which tells us
+	# which assets to download
 	eb deploy
+example:
+	# $ make example foo=bar
+	# >>> I say foo, you say 'bar'
+	echo "I say foo, you say '$(foo)'"
--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,13 @@
-import subprocess
+from pathlib import Path
 
+from django.conf import settings
 from django.shortcuts import render
 
 
 def home(request):
-    ps = subprocess.run(
-        args=["git", "rev-parse", "--short", "HEAD"],
-        stdout=subprocess.PIPE,
-        encoding="utf8"
-    )
+    head = Path(settings.BASE_DIR) / "HEAD"
+    with open(head) as f:
+        sha = f.read().strip()  # or f.read(40) to get the first 40 chars
 
-    sha = ps.stdout.strip()
-    assets_url = f"https://octo-waffle.s3.amazonaws.com/ebcustom-{sha}"
-
+    assets_url = f"https://octo-waffle.s3.amazonaws.com/ebcustom/{sha}"
     return render(request, "tasks/index.html", {"assets_url": assets_url})
