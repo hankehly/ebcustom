393f4f3c219897e237e92b034f16773294440870
Hank Ehly <henry.ehly@gmail.com>
2019-11-14 09:45:59 +0900

    Add more verbose logging for testjob

--- tasks/views.py
+++ tasks/views.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
 def home(request):
@@ -18,11 +19,20 @@ def home(request):
     return render(request, "tasks/index.html", {"assets_url": assets_url})
 
 
+@csrf_exempt
 def testjob(request):
     """
     handler for test job request
     """
     with open("/tmp/testjob.log", "a") as f:
-        f.write(f"testjob triggerd at {datetime.datetime.now()}\n")
+        f.write("\n\n")
+        f.write(f"* testjob triggerd at {datetime.datetime.now()}\n")
+
+        f.write("[headers]\n")
+        for key, value in request.headers.items():
+            f.write(f"- {key} : {value}\n")
+
+        f.write("[body]\n")
+        f.write(str(request.body))
 
     return HttpResponse(status=204)
